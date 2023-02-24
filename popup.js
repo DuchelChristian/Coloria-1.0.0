110a6c1b6f0452d08fa7e0b045d570e371f74b21
DuchelChristian <123276508+DuchelChristian@users.noreply.github.com>
2023-02-23 22:55:51 -0500

    The Javascript file
    Finally, this one was the hardest to get. You need first to get all your section and access to the storage. Then you create a function that displays the picked colors and that will be called anytime a color gets picked. Then you need to actually pick the colors via the color picker trough another function the tutorial helped us greatly with and finally you have to create the Clear function which was the easiest to do

---
+++ popup.js
@@ -0,0 +1,53 @@
+const colorPickerBtn = document.querySelector("#color-picker");
+const clearAll = document.querySelector(".clear-all");
+const colorList = document.querySelector(".all-colors");
+const pickedColors = JSON.parse(localStorage.getItem(".picked-colors")||"[]");
+
+
+//First we got all of our sections.
+
+const showColor = () => {
+    if(!pickedColors.length) return; 
+    colorList.innerHTML = pickedColors.map(color => `
+        <li class="color">
+            <span class="rect" style="background: ${color}; border: 1px solid ${color == "#ffffff" ? "#ccc": color}"></span>
+            <span class="value hex" data-color="${color}">${color}</span>
+        </li>
+    `).join(""); 
+    document.querySelector(".picked-colors").classList.remove("hide");
+  
+    document.querySelectorAll(".color").forEach(li => {
+        li.addEventListener("click", e => copyColor(e.currentTarget.lastElementChild));
+    });
+}
+showColor();
+
+//This function displays the selected colors by generating an li element for the list
+const activateEyeDropper = () => {
+    document.body.style.display = "none";
+    setTimeout(async () => {
+        try {
+            // Opening the eye dropper and getting the selected color
+            const eyeDropper = new EyeDropper();
+            const { sRGBHex } = await eyeDropper.open();
+            navigator.clipboard.writeText(sRGBHex);
+            // Adding the color to the list if it doesn't already exist
+            if(!pickedColors.includes(sRGBHex)) {
+                pickedColors.push(sRGBHex);
+                localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+                showColor();
+            }
+        } catch (error) {
+            alert("Failed to copy the color code!");
+        }
+        document.body.style.display = "block";
+    }, 10);
+}
+// Clearing all picked colors, updating local storage, and hiding the colorList element
+const clearAllColors = () => {
+    pickedColors.length = 0;
+    localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+    document.querySelector(".picked-colors").classList.add("hide");
+}
+clearAll.addEventListener("click", clearAllColors);
+colorPickerBtn.addEventListener("click", activateEyeDropper);
